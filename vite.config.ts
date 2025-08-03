c5079657166b82cc81a545cf69d69ddd989e0165
RishitTennis69 <135282797+RishitTennis69@users.noreply.github.com>
2025-08-03 12:01:28 -0700

    Remove speech time limit and add stopwatch

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      'three/examples/jsm': path.resolve('./node_modules/three/examples/jsm')
-    }
-  },
-  esbuild: {
-    target: 'es2022'
-  },
-  build: {
-    target: 'esnext'
-  },
-  optimizeDeps: {
-    include: [
-      'three/examples/jsm/nodes/display/BloomNode.js',
-      'three/examples/jsm/renderers/webgpu/WebGPURenderer.js'
-    ]
-  },
   server: {
     port: 3000,
     open: true
